c66ff0d70b91481eb523db7c90230fde91d2c632
Oswald Calvario <oswald.calvario@gmail.com>
2020-05-09 01:55:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ def save
   end
 
 def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
+    song = file_name.split(" - ")[0]
     artist = file_name.split(" - ")[0]
     new_song = self.new(song)
     new_song.artist_name = artist
