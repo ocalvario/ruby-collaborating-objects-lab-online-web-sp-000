6550d498ca64ad6f16bc3c00dafa040693e5965c
Oswald Calvario <oswald.calvario@gmail.com>
2020-05-09 02:05:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,11 +22,11 @@ class Song
       new_song = self.new(song)
       new_song.artist_name = artist
       new_song
-    end
+  end
   
-    def artist_name=(name)
-      self.artist = Artist.find_or_create_by_name(name)
-      artist.add_song(self)
-    end
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
 end
